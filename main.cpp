c4ef5d1f317986d28a22d720cce10c6e19974c32
Victor de Sousa Carrijo <victorcarrijo96@gmail.com>
2020-05-19 08:18:53 -0300

    Leitura do Volume

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
+        int compras;
+        double ticket;
         cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
         cout << "----------------------------" << endl;
         cout << "INFORME OS DADOS DO ÚLTIMO ANO" << endl;
+        cout << "Quantas compras o cliente fez no último ano? ";
+        cin >> compras;
+        cout << "Qual o ticket médio? ";
+        cin >> ticket;
 }
